805a62bc81f74135abf865fc5bd715f2b26910d3
Yuliia Podobrieieva <yuliia.podobrieieva@dev-pro.net>
2019-11-07 18:09:32 +0200

    add hbs, express

--- src/app.js
+++ src/app.js
@@ -1,11 +1,50 @@
+const request = require('request');
+const path = require('path');
 const express = require('express');
-console.log(__dirname);
-console.log(__filename);
+const hbs = require('hbs');
 
 const app = express();
+const publicDirectoryPath = path.join(__dirname, '../public');
+const pathViews = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+app.set('view engine', 'hbs')
+app.set('views', pathViews)
+hbs.registerPartials(partialsPath);
+
+
+app.use(express.static(publicDirectoryPath));
+
 
 app.get('', (req, res) => {
-    res.send('<h1> Weather </h1>')
+    res.render('index', {
+        title: 'ssdd',
+        name: 'Max',
+    })
+});
+
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'about',
+        name: 'TOo',
+    })
+});
+
+app.get('/products', (req, res) => {
+    if (req.query.search === undefined) {
+        return res.send({
+            error: 'Search is required',
+        })
+    }
+    
+    const url = 'https://api.darksky.net/forecast/02b00eb67db4a149a927536413d734a8/37.8267,-122.423?units=si'
+    
+    request({ url, json: true }, (err, resp) => {
+        res.send({
+            products: resp.body.currently.temperature,
+        })
+    })
 });
 
 app.get('/help', (req, res) => {
@@ -14,6 +53,17 @@ app.get('/help', (req, res) => {
         a: 156,
     });
 });
+
+app.get('/about/*', (req, res) => {
+    res.send('about not found')
+})
+
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        error: 'error'
+    })
+})
 
 app.listen(3000, () => {
     console.log('server 3000')
